8b2998f4e9d64b4eba942c785379f0d2d273b565
MostafaNazmulHasan <mostafanazmulhasan@gmail.com>
2023-06-12 20:51:24 +0600

    Display a List of Playlists

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useEffect } from 'react';
+import { Grid } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Container } from '@mui/system';
+import Navbar from './components/navbar';
+import PlaylistCardItem from './components/playlist-card-item';
 import usePlaylists from './hooks/usePlaylists';
 
 const App = () => {
-	const { getPlaylistById, playlists } = usePlaylists();
+	const { playlists, error, getPlaylistById } = usePlaylists();
 
-	useEffect(() => {
-		getPlaylistById('PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl');
-	}, []);
+	const playlistArray = Object.values(playlists);
 
-	console.log('Playlist', playlists);
+	console.log(playlists);
+	console.log(error);
 
-	return <div>App</div>;
+	return (
+		<>
+			<CssBaseline />
+			<Container maxWidth={'lg'} sx={{ my: 16 }}>
+				<Navbar getPlaylistById={getPlaylistById} />
+				{playlistArray.length > 0 && (
+					<Grid container alignItems="stretch">
+						{playlistArray.map((item) => (
+							<Grid item xs={12} md={6} lg={4} mb={2}>
+								<PlaylistCardItem
+									key={item.id}
+									playlistThumbnail={item.playlistThumbnail}
+									playlistTitle={item.playlistTitle}
+									channelTitle={item.channelTitle}
+								/>
+							</Grid>
+						))}
+					</Grid>
+				)}
+			</Container>
+		</>
+	);
 };
 
 export default App;
--- src/hooks/usePlayLists.jsx
+++ src/hooks/usePlayLists.jsx
@@ -1,78 +1,61 @@
 import { useState } from 'react';
 import getPlaylist from '../api';
-
 const usePlaylists = () => {
 	const [state, setState] = useState({
 		playlists: {},
 		recentPlaylists: [],
 		favorites: [],
 	});
+	const [error, setError] = useState('');
+	const [loading, setLoading] = useState(false);
 
 	const getPlaylistById = async (playlistId, force = false) => {
-
 		if (state.playlists[playlistId] && !force) {
 			return;
 		}
-		let result = await getPlaylist(playlistId)
-		let cid, ct;
-		result = result.map(item => {
-			const { channelId, title, description, thumbnails: { medium }, channelTitle, } = item.snippet;
 
-			if (!cid) {
-				cid = channelId
-			}
-
-			if (!ct) {
-				ct = channelTitle
-			}
-
-			return {
-				title,
-				description,
-				thumbnails: { medium },
-				contentDetails: item.contentDetails,
-			}
-		})
-		setState(prev => ({
-			...prev,
-			playlists: {
-				...prev.playlists,
-				[playlistId]: {
-					items: result,
-					playlistId: playlistId,
-					channelId: cid,
-					channelTitle: ct
+		setLoading(true);
+		try {
+			const playlist = await getPlaylist(playlistId);
+			setError('');
+			setState((prev) => ({
+				...prev,
+				playlists: {
+					...prev.playlists,
+					[playlistId]: playlist,
 				},
-			},
-		}))
-	}
-
-	const addToFavorites = (playlistId) => {
-		setState(prev => ({
-			...prev,
-			favorites: [...prev, playlistId]
-		}))
-	}
-
-	const addToRecent = (playlistId) => {
-		setState(prev => ({
-			...prev,
-			recentPlaylists: [...prev, playlistId]
-		}))
+			}));
+		} catch (e) {
+			setError(e.response?.data?.error?.message || 'Something went wrong');
+		} finally {
+			setLoading(false);
+		}
 	};
 
+	const addToFavorites = (playlistId) => {
+		setState((prev) => ({
+			...prev,
+			favorites: [...prev, playlistId],
+		}));
+	};
+	const addToRecent = (playlistId) => {
+		setState((prev) => ({
+			...prev,
+			recentPlaylists: [...prev, playlistId],
+		}));
+	};
 	const getPlaylistsByIds = (ids = []) => {
-		return ids.map(id => state.playlists[id])
-	}
-
+		return ids.map((id) => state.playlists[id]);
+	};
 	return {
 		playlists: state.playlists,
 		favorites: getPlaylistsByIds(state.favorites),
 		recentPlaylists: getPlaylistsByIds(state.recentPlaylists),
-		getPlaylistById, addToFavorites, addToRecent
-
-	}
-
+		error,
+		loading,
+		getPlaylistById,
+		addToRecent,
+		addToFavorites,
+	};
 };
-
 export default usePlaylists;
